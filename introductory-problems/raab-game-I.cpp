78e8b00d31bfb06c4d97f1fdb6502b232b6e3bf0
geniuspig2986 <geniuspig2986@gmail.com>
2026-05-08 18:15:39 -0700

    raab-game-1 start

---
+++ introductory-problems/raab-game-I.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<int> n;
+
+    int t;
+    cin >> t;
+    
+    for(int i = 0; i < n.size(); i ++){
+        cin >> n[i];
+    }
+
+    return 0;
+
+}
