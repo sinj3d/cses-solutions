1bb89fbc2bc058b093d12cd03619db4ff676131a
sinj3d <geniuspig2986@gmail.com>
2026-05-09 01:13:26 -0700

    raab game

--- introductory-problems/raab-game-I.cpp
+++ introductory-problems/raab-game-I.cpp
@@ -7,13 +7,45 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    vector<int> n;
-
     int t;
     cin >> t;
     
-    for(int i = 0; i < n.size(); i ++){
-        cin >> n[i];
+    for(int i = 0; i < t; i ++){
+        int n, a, b;
+
+        cin >> n;
+        cin >> a;
+        cin >> b;
+
+        if((a+b <= n) && (a == 0) == (b == 0)){
+
+            cout << "YES\n";
+            for(int c = n; c > 0; c--){
+                cout << c << " ";
+            }
+
+            cout << "\n";
+
+            for(int c = n; c > (a + b); c--){
+                cout << c << " ";
+            }
+
+            for(int c = (a + b); c > 0; c--){
+                if(c - b <= 0){
+                    cout << a + c << " ";
+                } else{
+                    cout << c - b << " ";
+                }
+            }
+
+            cout << "\n";
+
+        }else{
+
+            cout << "NO\n";
+            
+        }
+        
     }
 
     return 0;
